139a10967122cefd269e1c9c5b1a062a364d4d13
Sergio Minissale <minissale.sergio@outlook.itm>
2021-11-22 14:51:26 +0100

    esercizio finito

---
+++ src/org/generation/italy/strings/VerificaPalindrome.java
@@ -0,0 +1,35 @@
+package org.generation.italy.strings;
+import java.util.Scanner;
+
+public class VerificaPalindrome {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Inserisci la parola: ");
+		
+		String parola = scanner.nextLine();
+		String revParola = "";
+		
+		
+		//parola al contrario
+		for(int i = parola.length() - 1; i >= 0; i--) {
+			revParola = revParola + parola.charAt(i);	
+		}
+		System.out.println("La parola al contrario è: " + revParola);
+			
+			if(revParola.equals(parola)) {
+				System.out.println("La parola è palindroma: " + parola + " = " + revParola);
+				
+			} else {
+				System.out.println("La parola non è palindroma: " + parola + " != " + revParola);
+			}
+		
+	
+
+	
+	scanner.close();
+
+}
+}
